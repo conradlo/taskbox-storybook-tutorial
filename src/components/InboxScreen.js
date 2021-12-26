51f8dd413a9d7c062516377416516993a1e6a8cd
Conrad <email@conradlo.com>
2021-12-26 18:11:36 +0800

    feat: inbox screen component

---
+++ src/components/InboxScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+
+import { TaskList } from "./TaskList";
+
+const PureInboxScreen = ({ error }) => {
+  if (error) {
+    return (
+      <div className="page lists-show">
+        <div className="wrapper-message">
+          <span className="icon-face-sad" />
+          <div className="title-message">Oh no!</div>
+          <div className="subtitle-message">Something went wrong</div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="page lists-show">
+      <nav>
+        <h1 className="title-page">
+          <span className="title-wrapper">Taskbox</span>
+        </h1>
+      </nav>
+      <TaskList />
+    </div>
+  );
+};
+
+PureInboxScreen.propTypes = {
+  error: PropTypes.string,
+};
+
+PureInboxScreen.defaultProps = {
+  error: null,
+};
+
+const InboxScreen = () => {
+  const isError = useSelector((state) => state.isError);
+  return <PureInboxScreen error={isError} />;
+};
+
+export { PureInboxScreen, InboxScreen };
