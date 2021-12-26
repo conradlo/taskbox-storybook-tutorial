be4dd2aa9b5cda04c1c80c679e0918d6a6364073
Conrad <email@conradlo.com>
2021-12-26 23:44:48 +0800

    feat: inbox screen add interaction story

--- src/components/PureInboxScreen.stories.js
+++ src/components/PureInboxScreen.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { fireEvent, within } from "@storybook/testing-library";
 
 import { PureInboxScreen } from "./InboxScreen";
 
@@ -35,9 +36,17 @@ export default {
 const Template = (args) => <PureInboxScreen {...args} />;
 
 const Default = Template.bind({});
+
 const Error = Template.bind({});
 Error.args = {
   error: "Some error",
 };
 
-export { Default, Error };
+const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+  await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+};
+
+export { Default, Error, WithInteractions };
